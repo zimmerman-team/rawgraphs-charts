5bf066c09e930c7b4996d0baf2466ec91b22b9ad
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2023-06-15 12:11:59 +0200

    feat: implement big number chart

--- src/bigNumber/mapping.js
+++ src/bigNumber/mapping.js
@@ -3,13 +3,13 @@ import { getDimensionAggregator } from '@rawgraphs/rawgraphs-core'
 export function formatLargeAmountsWithPrefix(n) {
   if (!n) return ''
   if (Math.abs(Number(n)) >= 1.0e9) {
-    return `${(Math.abs(Number(n)) / 1.0e9).toFixed(2)} bln`
+    return `${(Math.abs(Number(n)) / 1.0e9).toFixed(2)}b`
   }
   if (Math.abs(Number(n)) >= 1.0e6) {
-    return `${(Math.abs(Number(n)) / 1.0e6).toFixed(2)} mln`
+    return `${(Math.abs(Number(n)) / 1.0e6).toFixed(2)}m`
   }
   if (Math.abs(Number(n)) >= 1.0e3) {
-    return `${(Math.abs(Number(n)) / 1.0e6).toFixed(2)} k`
+    return `${(Math.abs(Number(n)) / 1.0e6).toFixed(2)}k`
   }
   return `${formatFinancialValue(n)}`
 }
@@ -24,7 +24,9 @@ export const mapData = function (data, mapping, dataTypes, dimensions) {
   )
 
   return {
-    ...data[0],
+    title: mapping.title.value,
+    subtitle: mapping.subtitle.value,
+    description: mapping.description.value,
     value: formatLargeAmountsWithPrefix(
       sizeAggregator(data.map((d) => d[mapping.value.value]))
     ),
