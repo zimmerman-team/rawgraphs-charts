229380b8dee7dc78f47cd92aa9c328e668143b42
Psami-wondah <okechukwusamuel16@gmail.com>
2023-12-05 15:44:16 +0100

    feat: added echarts sunburst chart

---
+++ src/echartsSunburst/metadata.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  name: 'Echarts sunburst',
+  id: 'echarts.sunburst',
+  categories: ['hierarchies', 'proportions'],
+  description:
+    'It displays hierarchically structured data and a related quantitative dimension using concentric circles. The circle in the center represents the root node, with the hierarchies moving outward from the center. The angle of each arc corresponds to the qualitative dimension.',
+}
