c5bc1a1908b51afbbf1d5f00cf01be648ac00e19
Psami-wondah <okechukwusamuel16@gmail.com>
2023-12-18 08:32:46 +0100

    feat: Added Area time axis chart DX-928

---
+++ src/echartsAreatimeaxis/dimensions.js
@@ -0,0 +1,16 @@
+export const dimensions = [
+  {
+    id: 'x',
+    name: 'X Axis',
+    operation: 'get',
+    validTypes: ['date'],
+    required: true,
+  },
+  {
+    id: 'y',
+    name: 'Y Axis',
+    operation: 'get',
+    validTypes: ['number', 'date'],
+    required: true,
+  },
+]
