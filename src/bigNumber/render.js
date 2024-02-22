e93e0c5de47e09f0c0c1f5e2b9335b66b2308bc8
okorie2 <ellahokorie@gmail.com>
2024-02-22 23:01:22 +0100

    feat: logic for new BNC flow

--- src/bigNumber/render.js
+++ src/bigNumber/render.js
@@ -2,16 +2,16 @@ export function render(node, data) {
   node.innerHTML = `
     <div style="width: 317px; color: #000;">
       <div style="font-size: 21.985px;text-transform: uppercase;border-bottom: 1px solid #000;padding-bottom: 10px; font-weight:350;">
-        ${data.title ?? ''}
+        ${data.header ?? ''}
       </div>
       <div style="font-size: 105.527px;font-weight: 600;height: 112px;margin: 26px 0; display: flex; align-items: center;">
-        ${data.value ?? 0.00}
+        ${( data.metric) ?? 0.00}
       </div>
       <div style="font-size: 21.985px;text-transform: uppercase;border-bottom: 1px solid #000;padding-bottom: 10px; font-weight:350;">
-        ${data.subtitle ?? ''}
+        ${data.subheader ?? ''}
       </div>
       <div style="font-size: 17.588px;margin-top: 14px; font-weight:350;">
-        ${data.description ?? ''}
+        ${data.unitofmeasurement ?? ''}
       </div>
     </div>
   `
